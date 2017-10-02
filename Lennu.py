f10884b63eb9b75f6f0f8e0943d2981a5b8ad8a4
Tonuser <heil.tolerance@gmail.com>
2017-10-02 19:06:17 +0300

    Added level system

--- Lennu.py
+++ Lennu.py
@@ -4,6 +4,7 @@ import asyncio
 
 from cog.DataController import DataController
 from cog.Mandunu import Mandunu
+from cog.LevelSystem import LevelSystem
 
 class Lennu:
     bot = None
@@ -13,7 +14,13 @@ class Lennu:
         bot.add_cog(self)
 
         data_controller = DataController(bot)
+        level_system = LevelSystem(bot, data_controller)
+        level_system.add_level("esimene_tase", 70)
+        level_system.add_level("teine_tase", 140)
+        level_system.add_level("kolmas_tase", 200)
+        level_system.add_level("neljas_tase", 280)
         mandunu = Mandunu(bot, data_controller)
+
 
     async def on_message(self, message):
 
--- cog/LevelSystem.py
+++ cog/LevelSystem.py
@@ -1,3 +1,42 @@
+import discord
+from discord.ext import commands
+
+
+class LevelSystem:
+    """Ayy lmao dude"""
+    bot = None
+    data = None
+    levels = {}
+
+    def __init__(self, bot, data_controller):
+        self.bot = bot
+        bot.add_cog(self)
+        self.data = data_controller
+
+    def add_level(self, name, min_chars):
+        self.levels[name] = self.Level(min_chars)
+
+    async def on_message(self, message):
+        if str(message.channel) in self.levels:
+            if self.levels[str(message.channel)].is_valid(message.content) is False:
+                await self.bot.send_message(
+                    message.author,
+                    "Su sõnum ei vastanud kanali " + str(message.channel) + " reeglitele: \n```" +
+                    message.content + "```")
+                await self.bot.delete_message(message)
+
+    class Level:
+        def __init__(self, min_chars):
+            self.min_chars = min_chars
+
+        def is_valid(self, content):
+            if len(content) < self.min_chars:
+                return False
+
+            return True
+
+
+
 
 '''limit = 0
 if str(message.channel) == 'kolmas_tase':
